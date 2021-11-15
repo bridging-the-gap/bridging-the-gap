59b334df20344db9fde5beee26e95ddef900bd44
Leilani Reich <reichld@hawaii.edu>
2021-11-14 22:19:56 -1000

    Finished replicating most of figma mockup screenshot.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,57 +1,55 @@
 import React from 'react';
-import { Grid, Image, Container, Header } from 'semantic-ui-react';
+import { Container, FormTextArea, FormButton, Grid, Header, List, ListItem } from 'semantic-ui-react';
 
 /** Renders a color-blocked static landing page. */
 class Landing extends React.Component {
   render() {
     return (
       <div id="landing-page">
-        <div className='landing-green-background'>
+        <div className='landing-blue-background'>
           <Container textAlign='center'>
-            <Header style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }} as='h1'>
-                Welcome to Bowfolios
-            </Header>
-            <Header style={{ paddingBottom: '20px', color: 'white' }} as='h3'>
-                Profiles, projects, and interest areas for the UH Community
+            <Header style={{ paddingTop: '100px', paddingBottom: '100px', color: 'white', fontSize: '40pt', fontFamily: 'roboto' }} as='h1'>
+                Bridging The Gap
             </Header>
           </Container>
         </div>
-        <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Start by making your profile....</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/home-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/profiles-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
-        <div className='landing-green-background'>
-          <Header style={{ color: 'white' }} as='h2' textAlign='center'>...then add your projects</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/add-project-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/projects-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
-        <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>
-              Connect to people and projects with shared interests!
-          </Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/interests-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/filter-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
 
+        <div className='landing-white-background'>
+
+          <Container textAlign='center' style={{ width: '700px' }}>
+            <Header style={{ paddingTop: '50px', paddingBottom: '50px', color: '#376551', fontSize: '20px' }} as='h3'>
+              <List>
+                <ListItem className='text-spacing'>
+                  <p>The Company Connector web application provides a new way for local and non-local
+                    companies who want to recruit students from UH to make their (potential)
+                    opportunities known to students.</p>
+                </ListItem>
+                <ListItem className='text-spacing'>
+                  <p>At the same time, students can create profiles on the site with their
+                    interests. The site can match students to employers and vice-versa.</p>
+                </ListItem>
+                <ListItem className='text-spacing'>
+                  <p>
+                    To get started, click the sign up link in the top right.
+                  </p>
+                </ListItem>
+              </List>
+            </Header>
+          </Container>
+        </div>
+        <div className='landing-blue-background'>
+          <Container align='center' style={{ paddingTop: '30px', paddingBottom: '80px' }}>
+            <Header as='h3' textAlign='center' inverted>
+            Have feedback?
+            </Header>
+            <Grid centered columns='1'>
+              <Grid.Row >
+                <FormTextArea id='feedback' name='feedback' placeholder='Let us know what you think!' style={{ width: '500px' }}/>
+                <FormButton type='submit' size='huge' style={{ float: 'right' }}>Send</FormButton>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </div>
       </div>
 
     );
