28e3e2c0104c0815128d00d3e6e862a381f45a65
Leilani Reich <reichld@hawaii.edu>
2021-11-15 14:16:00 -1000

    Gave sin@hawaii.edu role of admin, added admin route to App.jsx, created admin page, added admin link to navbar.

---
+++ app/imports/ui/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <Container id='admin-page' centered>
+        Hi
+      </Container>
+    );
+  }
+}
+
+export default Admin;
