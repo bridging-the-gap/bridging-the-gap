c1a8abb7aed657f4c356760ed4d2e9e1de889210
Leilani Reich <reichld@hawaii.edu>
2021-12-07 10:49:21 -1000

    Added ProfilesEvents and ProfilesJobs

---
+++ app/imports/api/profiles/ProfilesEvents.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class ProfilesEventsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProfilesEventsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      // profile is the user's email.
+      profile: String,
+      // event is the eventName.
+      event: String,
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const ProfilesEvents = new ProfilesEventsCollection();
